05a638e3b68e9c720946e611ff39c37af828e4a5
Dongju <ldj0635@gmail.com>
2019-10-28 04:12:06 +0000

    Updates

---
+++ precache-manifest.0ee470c409d816310422f51353219190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a81a29cdfbef002d9c0314c37d1e9e6",
+    "url": "/djportfolio/index.html"
+  },
+  {
+    "revision": "4b8e2b7e4a741eb4788b",
+    "url": "/djportfolio/static/css/main.c7b7a933.chunk.css"
+  },
+  {
+    "revision": "d6e3e62c6d6f7879ceb3",
+    "url": "/djportfolio/static/js/2.df5102b6.chunk.js"
+  },
+  {
+    "revision": "4b8e2b7e4a741eb4788b",
+    "url": "/djportfolio/static/js/main.755de6c1.chunk.js"
+  },
+  {
+    "revision": "86b07c62edda3e876ff9",
+    "url": "/djportfolio/static/js/runtime-main.e204f11a.js"
+  }
+]);
